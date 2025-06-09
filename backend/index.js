45470fecb05e89ee93442e228fa969ee5744a357
shaugrin <93257010+shaugrin@users.noreply.github.com>
2025-06-09 18:59:09 +0530

    Fixed cookie issue

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
